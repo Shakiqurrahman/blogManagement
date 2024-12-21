67bace5289b4fbae18ee4398ec3fe121d5556ae5
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-12-21 15:24:21 +0600

    refactor : blogs only user can create

--- src/modules/Blog/blogRoute.ts
+++ src/modules/Blog/blogRoute.ts
@@ -5,21 +5,9 @@ import { blogControllers } from './blogController';
 
 const router = Router();
 
-router.post(
-    '/',
-    auth(USER_ROLE.user, USER_ROLE.admin),
-    blogControllers.createBlog,
-);
-router.patch(
-    '/:id',
-    auth(USER_ROLE.user, USER_ROLE.admin),
-    blogControllers.updateBlog,
-);
-router.delete(
-    '/:id',
-    auth(USER_ROLE.user, USER_ROLE.admin),
-    blogControllers.deleteBlog,
-);
+router.post('/', auth(USER_ROLE.user), blogControllers.createBlog);
+router.patch('/:id', auth(USER_ROLE.user), blogControllers.updateBlog);
+router.delete('/:id', auth(USER_ROLE.user), blogControllers.deleteBlog);
 router.get('/', blogControllers.getAllBlogs);
 
 export const blogRoutes = router;
